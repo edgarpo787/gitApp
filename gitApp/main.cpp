58cad98ef54bc15a2549a9d1702724b58936a1ae
edgarpo <edgarpo787@gmail.com>
2017-05-10 10:22:27 +0600

    final

--- gitApp/main.cpp
+++ gitApp/main.cpp
@@ -1,12 +1,41 @@
 #include<iostream>
 #include<conio.h>
+#include<Windows.h>
 
 using namespace std;
+int 
 
 int main() {
+	/*SetConsoleOutputCP(1251);
+	int n = 0;//кол-во элементов в массиве
+	cout << "¬ведите кол-во элементов в массиве:";
+	cin >> n;
+	double* array = new double[n];
+	int counter = 0;
+	double* array2 = new double[n];
+	for (int i = 0,j=0; i < n; i++) {
+		cin >> array[i];
+		if (array[i] >= 0) {
+			array2[j] = array[i];
+			counter++;
+			j++;
+			}
+
+	}
+	
+	for (int i = 0,j1=counter; i < n; i++) {
+		if (array[i] < 0) {
+			array2[j1] = array[i];
+			j1++;
+		}
+		
+		
+	}
+	for (int i = 0; i < n; i++) {
+		cout << array2[i] << endl;
+	}*/
 
 
-	//comment
 	_getch();
 	return 0;
 }
